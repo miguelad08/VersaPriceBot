611a05f7d7247b9f867caa6f95f94acdc1f7335b
Miguel Angel Del Cid <miguelangeldelcid@Stacys-MacBook-Air.local>
2022-08-24 20:30:29 -0700

    Update xVersa Price ratio

--- index.js
+++ index.js
@@ -12,12 +12,15 @@ function getPrices() {
         // If we got a valid response
         if (res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
             let currentPrice = res.data[0].current_price || 0 // Default to zero
-            let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+
+            // Price change in percentage ------ for later use if needed
+            // let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
+
             let symbol = res.data[0].symbol || '?'
             // Unix code for arrow pointing up and to the right  let northEastArrow = '\u2197'
             client.user.setPresence({
                 activities: [{
-                    name: "1 xVERSA ≈ 1.117 VERSA",//`24h: ${priceChange.toFixed(2)}%`
+                    name: "1 xVERSA ≈ 1.121 VERSA", //`24h: ${priceChange.toFixed(2)}%`
                     type: "WATCHING"
                 }]
             })
